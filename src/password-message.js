82b3af322ca5f2ac5cfc0d7525ed54cdc245ad93
FSpark <stardust@fspark.me>
2023-05-02 19:40:36 +0800

    fix: .gitignore

--- src/password-message.js
+++ src/password-message.js
@@ -2,8 +2,8 @@
     /*jslint node: true, browser: true */
     /*global $tw: false */
     "use strict";
-//h
-// Export name and synchronous status
+
+    // Export name and synchronous status
     exports.name = "cse-password";
     exports.platforms = ["browser"];
     exports.after = ["startup"];
@@ -21,7 +21,6 @@
             if(params && params.password) {
                 password = params.password;
             }
-// debugger;
             if(password){
                     $tw.CSE.setPassword(password);
                     $tw.CSE.forcePush();
@@ -60,27 +59,6 @@
         $tw.rootWidget.addEventListener("tm-cse-forget-password", function (event) {
             if($tw.browser) {
                 $tw.CSE.forgetPassword();
-            }
-        });
-        $tw.rootWidget.addEventListener("tm-cse-modal-test", function (event) {
-            if($tw.browser) {
-                var id = $tw.wiki.getTiddlerText('$:/temp/CSE-IntervalID')
-                var n = 0
-                if(id) clearTimeout(parseInt(id))
-                var self = this;
-                $tw.modal.display("$:/plugins/FSpark/TW5-CSE/ui/PushingModal")
-                id = setInterval(function() {
-                    if(n<5){
-                        $tw.wiki.addTiddler({title: "$:/temp/shownumber",text: n.toString()})
-                    }else{
-                        // debugger;
-                        $tw.wiki.addTiddler({title: "$:/state/cse-modal-close", text: "yes"})
-                        clearTimeout(id)
-                        $tw.wiki.deleteTiddler("$:/temp/CSE-IntervalID")
-                    }
-                    n++;
-                }, 1000);
-                $tw.wiki.addTiddler({title: "$:/temp/CSE-IntervalID",text: id.toString()})
             }
         });
         // Ensure that $:/isCSEncrypted is maintained properly
